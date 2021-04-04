5bb408463647a406b083ec823b5ff3acfcd35c77
leung123 <leung123msn@hotmail.com>
2021-04-04 17:46:51 +0800

    Add initial blog post listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
 import { graphql, useStaticQuery } from 'gatsby';
-
+import BlogList from '../components/BlogList';
 import Layout from '../components/Layout';
-
 import styles from './index.module.css';
 
 export default function IndexPage() {
@@ -16,12 +14,12 @@ export default function IndexPage() {
             }
         }
     `);
-
     return (
         <Layout>
             <div id={styles.hero}>
                 <h1>{data.site.siteMetadata.title}</h1>
             </div>
+            <BlogList />
         </Layout>
     );
 }
