08d4f9191db7f01f49943abba092a9a8a05d49f1
leung123 <leung123msn@hotmail.com>
2021-04-04 12:41:40 +0800

    Add initial configuration

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,16 +2,14 @@ import React from 'react';
 import styles from './Layout.module.css';
 
 export default function Layout({ children }) {
-  return (
-    <div>
-      <header id={styles.header}>
-        <div id={styles.inner}>
-          <h1>Joe's Coffee Shop</h1>
+    return (
+        <div>
+            <header id={styles.header}>
+                <div id={styles.inner}>
+                    <h1>Joe's Coffee Shop</h1>
+                </div>
+            </header>
+            <main id={styles.main}>{children}</main>
         </div>
-      </header>
-      <main id={styles.main}>
-        {children}
-      </main>
-    </div>
-  );
+    );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,21 @@ import Layout from '../components/Layout';
 import styles from './index.module.css';
 
 export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-  {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`);
+    const data = useStaticQuery(graphql`
+        {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `);
 
-  return (
-    <Layout>
-      <div id={styles.hero}>
-        <h1>{data.site.siteMetadata.title}</h1>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div id={styles.hero}>
+                <h1>{data.site.siteMetadata.title}</h1>
+            </div>
+        </Layout>
+    );
 }
-
