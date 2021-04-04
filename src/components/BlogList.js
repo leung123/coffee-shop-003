3564d239802591cf69f1feb85141bdac13b14a6f
leung123 <leung123msn@hotmail.com>
2021-04-04 19:13:55 +0800

    Sort blog posts by date

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,7 +5,9 @@ import BlogPost from './BlogPost';
 export default function BlogList() {
     const data = useStaticQuery(graphql`
         {
-            allMarkdownRemark {
+            allMarkdownRemark(
+                sort: { fields: frontmatter___date, order: DESC }
+            ) {
                 edges {
                     node {
                         id
